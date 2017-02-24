69de00a86caa8335ff78ced273e9cf0a1d88ec2b
EasonX <yixinx@andrew.cmu.edu>
2017-02-23 22:22:33 -0800

    Add simple Ui for the first screen

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    @IBAction func getLocation() {
+        // do nothing yet
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()

6c7bb680bcc1299c63371a845aff8b273260075e
EasonX <yixinx@andrew.cmu.edu>
2017-02-24 16:47:20 -0800

    Handling kinds of errors

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -19,19 +19,31 @@ class CurrentLocationViewController: UIViewController , CLLocationManagerDelegat
     @IBOutlet weak var getButton: UIButton!
     
     var location: CLLocation?
+    var updatingLocation = false
+    var lastLocationError: Error?
     let locationManager = CLLocationManager()
     
     // MARK: - CLLocationManagerDelegate
     func locationManager(_ manager: CLLocationManager,
                          didFailWithError error: Error) {
         print("didFailWithError \(error)")
+        
+        if(error as NSError).code == CLError.locationUnknown.rawValue{
+            return
+        }
+        lastLocationError = error
+        
+        stopLocationManager()
+        updateLabels()
     }
+    
     func locationManager(_ manager: CLLocationManager,
                          didUpdateLocations locations: [CLLocation]) {
         let newLocation = locations.last!
         print("didUpdateLocations \(newLocation)")
         
         location = newLocation
+        lastLocationError = nil
         updateLabels()
     }
     
@@ -47,9 +59,8 @@ class CurrentLocationViewController: UIViewController , CLLocationManagerDelegat
             return
         }
         
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        locationManager.startUpdatingLocation()
+        startLocationManager()
+        updateLabels()
     }
 
     override func viewDidLoad() {
@@ -81,7 +92,39 @@ class CurrentLocationViewController: UIViewController , CLLocationManagerDelegat
             longitudeLabel.text = ""
             addressLabel.text = ""
             tagButton.isHidden = true
-            messageLabel.text = "Tap 'Het My Location' to Start"
+            
+            let statusMessage: String
+            if let error = lastLocationError as? NSError{
+                if error.domain == kCLErrorDomain && error.code == CLError.denied.rawValue{
+                    statusMessage = "Location Services Disabled"
+                }else{
+                    statusMessage = "Error Getting Location"
+                }
+            }else if !CLLocationManager.locationServicesEnabled(){
+                statusMessage = "Location Services Disabled"
+            }else if updatingLocation{
+                statusMessage = "Searching..."
+            }else{
+                statusMessage = "Tap 'Het My Location' to Start"
+            }
+            messageLabel.text = statusMessage
+        }
+    }
+    
+    func startLocationManager(){
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            updatingLocation = true
+        }
+    }
+    
+    func stopLocationManager(){
+        if updatingLocation{
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            updatingLocation = false
         }
     }
 }

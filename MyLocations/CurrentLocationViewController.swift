d0e46a68cbb478179d0a8d8f19762fc244b63c4f
EasonX <yixinx@andrew.cmu.edu>
2017-02-28 14:46:02 -0800

    Put the location info into the location detail screen

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -255,5 +255,14 @@ class CurrentLocationViewController: UIViewController , CLLocationManagerDelegat
             configureGetButton()
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TagLocation"{
+            let navigationController = segue.destination as! UINavigationController
+            let controller = navigationController.topViewController as! LocationDetailsViewController
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
+        }
+    }
 }
 
--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -7,6 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
+
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    return formatter
+}()
 
 class LocationDetailsViewController: UITableViewController{
 
@@ -16,6 +24,9 @@ class LocationDetailsViewController: UITableViewController{
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
+    
     
     @IBAction func done(){
         dismiss(animated: true, completion: nil)
@@ -25,7 +36,48 @@ class LocationDetailsViewController: UITableViewController{
         dismiss(animated: true, completion: nil)
     }
 
-
-
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        
+        if let placemark = placemark {
+            addressLabel.text = string(from: placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+        
+        dateLabel.text = format(date: Date())
+    }
+    
+    // Help function
+    func string(from placemark: CLPlacemark) -> String {
+        var text = ""
+        if let s = placemark.subThoroughfare {
+            text += s + " " }
+        if let s = placemark.thoroughfare {
+            text += s + ", "
+        }
+        if let s = placemark.locality {
+            text += s + ", "
+        }
+        if let s = placemark.administrativeArea {
+            text += s + " " }
+        if let s = placemark.postalCode {
+            text += s + ", "
+        }
+        if let s = placemark.country {
+            text += s }
+        return text
+    }
+    
+    // "Lazy Loading"
+    // call private dateFormatter the first time format is called
+    func format(date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
 }

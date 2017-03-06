bd2adf8b9f5fde88a07fca54e9f2ee95ab34800e
EasonX <yixinx@andrew.cmu.edu>
2017-03-05 23:01:39 -0800

    Add a new property to get the object from the persistent container, there's a bug, no idea

--- MyLocations/AppDelegate.swift
+++ MyLocations/AppDelegate.swift
@@ -24,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext = self.persistentContainer.viewContext
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
